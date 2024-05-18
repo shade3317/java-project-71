099626384f3cc00e237640e8163e1698f398815e
Admin <shade641114@gmail.com>
2024-05-18 19:36:40 +0300

    Exercise 9-10 1.4

--- app/src/test/java/DifferTest.java
+++ app/src/test/java/DifferTest.java
@@ -8,56 +8,64 @@ import org.junit.jupiter.api.Test;
 
 
 public class DifferTest {
-    private static String resultStylish1_2;
-    private static String resultStylish3_4;
-    private static String resultPlain3_4;
-    private static String resultJson3_4;
+    private static String resultStylish12;
+    private static String resultStylish34;
+    private static String resultPlain34;
+    private static String resultJson34;
 
-    private static final String pathResultStylish1_2 = "src/test/resources/TestStylish1_2";
-    private static final String pathResultStylish3_4 = "src/test/resources/TestStylish3_4";
-    private static final String pathResultPlain3_4   = "src/test/resources/TestPlain3_4";
-    private static final String pathResultJson3_4    = "src/test/resources/TestJson3_4";
-
-    private static final String pathFile1_json = "src/test/resources/file1.json";
-    private static final String pathFile2_json = "src/test/resources/file2.json";
-    private static final String pathFile3_json = "src/test/resources/file3.json";
-    private static final String pathFile4_json = "src/test/resources/file4.json";
-
-    private static final String pathFile1_yml  = "src/test/resources/file1.yml";
-    private static final String pathFile2_yml  = "src/test/resources/file2.yml";
-    private static final String pathFile3_yml  = "src/test/resources/file3.yml";
-    private static final String pathFile4_yml  = "src/test/resources/file4.yml";
+    private static String pathFile1json;
+    private static String pathFile2json;
+    private static String pathFile3json;
+    private static String pathFile4json;
+    private static String pathFile1yml;
+    private static String pathFile2yml;
+    private static String pathFile3yml;
+    private static String pathFile4yml;
 
 
 
     @BeforeAll
     public static void beforeAll() throws Exception {
-        resultStylish1_2 = Utils.getDataFromFilePath(pathResultStylish1_2);
-        resultStylish3_4 = Utils.getDataFromFilePath(pathResultStylish3_4);
-        resultPlain3_4   = Utils.getDataFromFilePath(pathResultPlain3_4);
-        resultJson3_4    = Utils.getDataFromFilePath(pathResultJson3_4);
+        pathFile1json = "src/test/resources/file1.json";
+        pathFile2json = "src/test/resources/file2.json";
+        pathFile3json = "src/test/resources/file3.json";
+        pathFile4json = "src/test/resources/file4.json";
+        pathFile1yml  = "src/test/resources/file1.yml";
+        pathFile2yml  = "src/test/resources/file2.yml";
+        pathFile3yml  = "src/test/resources/file3.yml";
+        pathFile4yml  = "src/test/resources/file4.yml";
+
+        String pathResultStylish12 = "src/test/resources/TestStylish1_2";
+        String pathResultStylish34 = "src/test/resources/TestStylish3_4";
+        String pathResultPlain34   = "src/test/resources/TestPlain3_4";
+        String pathResultJson34    = "src/test/resources/TestJson3_4";
+
+        resultStylish12 = Utils.getDataFromFilePath(pathResultStylish12);
+        resultStylish34 = Utils.getDataFromFilePath(pathResultStylish34);
+        resultPlain34   = Utils.getDataFromFilePath(pathResultPlain34);
+        resultJson34    = Utils.getDataFromFilePath(pathResultJson34);
     }
 
     @Test
     public void generateTestStylish() throws Exception {
-        assertThat(Differ.generate(pathFile1_json, pathFile2_json)).isEqualTo(resultStylish1_2);
-        assertThat(Differ.generate(pathFile3_json, pathFile4_json)).isEqualTo(resultStylish3_4);
+        assertThat(Differ.generate(pathFile1json, pathFile2json)).isEqualTo(resultStylish12);
+        assertThat(Differ.generate(pathFile3json, pathFile4json)).isEqualTo(resultStylish34);
 
-        assertThat(Differ.generate(pathFile1_yml,  pathFile2_yml)).isEqualTo(resultStylish1_2);
-        assertThat(Differ.generate(pathFile3_yml,  pathFile4_yml)).isEqualTo(resultStylish3_4);
+        assertThat(Differ.generate(pathFile1yml,  pathFile2yml)).isEqualTo(resultStylish12);
+        assertThat(Differ.generate(pathFile3yml,  pathFile4yml)).isEqualTo(resultStylish34);
     }
 
     @Test
     public void generateTestPlain() throws Exception {
-        assertThat(Differ.generate(pathFile3_json, pathFile4_json, "plain")).isEqualTo(resultPlain3_4);
+        assertThat(Differ.generate(pathFile3json, pathFile4json, "plain")).isEqualTo(resultPlain34);
 
-        assertThat(Differ.generate(pathFile3_yml,  pathFile4_yml,  "plain")).isEqualTo(resultPlain3_4);
+        assertThat(Differ.generate(pathFile3yml,  pathFile4yml,  "plain")).isEqualTo(resultPlain34);
     }
 
     @Test
     public void generateTestJson() throws Exception {
-        assertThat(Differ.generate(pathFile3_json, pathFile4_json, "json")).isEqualTo(resultJson3_4);
+        assertThat(Differ.generate(pathFile3json, pathFile4json, "json")).isEqualTo(resultJson34);
 
-        assertThat(Differ.generate(pathFile3_yml,  pathFile4_yml,  "json")).isEqualTo(resultJson3_4);
+        assertThat(Differ.generate(pathFile3yml,  pathFile4yml,  "json")).isEqualTo(resultJson34);
     }
 }

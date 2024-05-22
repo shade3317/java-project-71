c66567bbe06044221155325b8c7a553a1d723aa5
Admin <shade641114@gmail.com>
2024-05-22 15:20:00 +0300

    Comment edits 1.4

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -1,5 +1,6 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import hexlet.code.formatters.Stylish;
 import hexlet.code.formatters.Plain;
 import hexlet.code.formatters.Json;
@@ -8,12 +9,12 @@ import java.util.List;
 import java.util.Map;
 
 public class Formatter {
-    public static String generateResult(List<Map<String, Object>> diffs, String format) throws Exception {
+    public static String generateResult(List<Map<String, Object>> diffs, String format) throws JsonProcessingException {
         return switch (format) {
             case "stylish" -> Stylish.generateResult(diffs);
             case "plain"   -> Plain.generateResult(diffs);
             case "json"    -> Json.generateResult(diffs);
-            default -> throw new Exception("Unknown format");
+            default -> throw new RuntimeException("Unknown format");
         };
     }
 }
